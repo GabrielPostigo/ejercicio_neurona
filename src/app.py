57d1e6b57b67a5a34609c1e52437a2d2cef902f8
Postigo Rando <g.postigo.rando@accenture.com>
2024-01-18 10:01:03 +0100

    Reparaciones varias

--- src/app.py
+++ src/app.py
@@ -11,32 +11,32 @@ st.image('imagen_neurona.jpg', caption=None)
 st.title('!Hola neurona¡')
 
 with tab1:
-    st.markdown('Peso w<sub>0</sub>', unsafe_allow_html=True)
-    w0 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
-    st.markdown('Entrada x<sub>0</sub>', unsafe_allow_html=True)
-    x0 = st.number_input(step=1)
+    # st.markdown('Peso w<sub>0</sub>', unsafe_allow_html=True)
+    w0 = st.slider('Peso',0,0.0,5.0)
+    # st.markdown('Entrada x<sub>0</sub>', unsafe_allow_html=True)
+    x0 = st.number_input("Introduzca el valor de la entrada")
     y = w0*x0
 
-    if st.button('Calcular la salida'):
+    if st.button('Calcular la salida','b1'):
         st.text(f'La salida de la neurona es {y}')
 
 with tab2:
     col1,col2 = st.columns(2)
     with col1:
         st.markdown('Peso w<sub>0</sub>', unsafe_allow_html=True)
-        w01 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
+        w01 = st.slider('s1',0.0,5.0, label_visibility='collapsed')
         st.markdown('Entrada x<sub>0</sub>', unsafe_allow_html=True)
-        x01 = st.number_input(step=1)
+        x01 = st.number_input('i1',step=1)
     
     with col2:
         st.markdown('Peso w<sub>1</sub>', unsafe_allow_html=True)
-        w1 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
+        w1 = st.slider('s2',0,0.0,5.0, label_visibility='collapsed')
         st.markdown('Peso x<sub>1</sub>', unsafe_allow_html=True)
-        x1 = st.number_input(step=1)
+        x1 = st.number_input('i2',step=1)
     
     y1 = (w01*x01)+(w1*x1)
 
-    if st.button('Calcular la salida'):
+    if st.button('Calcular la salida','b2'):
         st.text(f'La salida de la neurona es {y1}')
 
 
@@ -44,25 +44,25 @@ with tab3:
     col11,col21,col3 = st.columns(3)
     with col11:
         st.markdown('Peso w<sub>0</sub>', unsafe_allow_html=True)
-        w02 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
+        w02 = st.slider('s3',0,0.0,5.0, label_visibility='collapsed')
         st.markdown('Entrada x<sub>0</sub>', unsafe_allow_html=True)
-        x02 = st.number_input(step=1)
+        x02 = st.number_input('i3',step=1)
     
     with col21:
         st.markdown('Peso w<sub>1</sub>', unsafe_allow_html=True)
-        w12 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
+        w12 = st.slider('s4',0,0.0,5.0, label_visibility='collapsed')
         st.markdown('Entrada x<sub>1</sub>', unsafe_allow_html=True)
-        x12 = st.number_input(step=1)
+        x12 = st.number_input('i4',step=1)
     
     with col3:
         st.markdown('Peso w<sub>2</sub>', unsafe_allow_html=True)
-        w2 = st.slider('',0,0.0,5.0, label_visibility='collapsed')
+        w2 = st.slider('s5',0,0.0,5.0, label_visibility='collapsed')
         st.markdown('Entrada x<sub>2</sub>', unsafe_allow_html=True)
-        x2 = st.number_input(step=1)
+        x2 = st.number_input('i5',step=1)
 
-    b = st.number_input('Introduzca el valor del sesgo', step=1)
+    b = st.number_input('Introduzca el valor del sesgo')
 
     y2 = (x02*w02)+(x12*w12)+(x2*w2)+b
 
-    if st.button('Calcular la salida'):
+    if st.button('Calcular la salida','b3'):
         st.text(f'La salida de la neurona es {y2}')
